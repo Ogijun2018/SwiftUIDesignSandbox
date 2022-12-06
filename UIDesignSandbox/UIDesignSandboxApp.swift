219f867dcc43502193885b730e237b19a26443d3
Jun Ogino <ogijun.mk@hotmail.co.jp>
2022-12-06 18:08:17 +0900

    Initial Commit

---
+++ UIDesignSandbox/UIDesignSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIDesignSandboxApp.swift
+//  UIDesignSandbox
+//
+//  Created by jun.ogino on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct UIDesignSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
